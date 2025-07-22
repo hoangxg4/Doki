20a6cc1b8471d87edc075033b97a1677748527f3
dragonx943 <premieregirl26@gmail.com>
2025-07-22 22:58:03 +0700

    [Kotatsu + Doki] Update parsers / exts

--- app/src/main/kotlin/org/dokiteam/doki/core/parser/DummyParser.kt
+++ app/src/main/kotlin/org/dokiteam/doki/core/parser/DummyParser.kt
@@ -6,12 +6,12 @@ import org.dokiteam.doki.parsers.config.ConfigKey
 import org.dokiteam.doki.parsers.core.AbstractMangaParser
 import org.dokiteam.doki.parsers.model.Manga
 import org.dokiteam.doki.parsers.model.MangaChapter
+import org.dokiteam.doki.parsers.model.MangaListFilter
+import org.dokiteam.doki.parsers.model.MangaListFilterCapabilities
 import org.dokiteam.doki.parsers.model.MangaListFilterOptions
 import org.dokiteam.doki.parsers.model.MangaPage
 import org.dokiteam.doki.parsers.model.MangaParserSource
 import org.dokiteam.doki.parsers.model.SortOrder
-import org.dokiteam.doki.parsers.model.search.MangaSearchQuery
-import org.dokiteam.doki.parsers.model.search.MangaSearchQueryCapabilities
 import java.util.EnumSet
 
 /**
@@ -25,14 +25,18 @@ class DummyParser(context: MangaLoaderContext) : AbstractMangaParser(context, Ma
 	override val availableSortOrders: Set<SortOrder>
 		get() = EnumSet.allOf(SortOrder::class.java)
 
-	override val searchQueryCapabilities: MangaSearchQueryCapabilities
-		get() = MangaSearchQueryCapabilities()
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = MangaListFilterCapabilities()
 
 	override suspend fun getDetails(manga: Manga): Manga = stub(manga)
 
 	override suspend fun getFilterOptions(): MangaListFilterOptions = stub(null)
 
-	override suspend fun getList(query: MangaSearchQuery): List<Manga> = stub(null)
+	override suspend fun getList(
+		offset: Int,
+		order: SortOrder,
+		filter: MangaListFilter
+	): List<Manga> = stub(null)
 
 	override suspend fun getPages(chapter: MangaChapter): List<MangaPage> = stub(null)
 
